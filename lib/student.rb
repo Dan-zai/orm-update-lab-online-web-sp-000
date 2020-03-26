d18420477efd1240599d93c613be345874c8ec48
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-26 05:55:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,10 @@ class Student
     student
   end
   
-  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 
 end
